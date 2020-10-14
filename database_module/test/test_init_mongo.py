d444a35d38a92ab3e984b362e401f45ffbf359b5
Mikko Seppälä <mikko.seppala@gofore.com>
2020-10-14 13:52:42 +0300

    OPMONDEV-9 init_mongo.py unit test: admin user generation advanced

--- database_module/test/test_init_mongo.py
+++ database_module/test/test_init_mongo.py
@@ -21,9 +21,32 @@ class TestInitMongo(unittest.TestCase):
         client.admin.command.assert_called()
         self.assertEqual(client.admin.command.call_count, 3)
         self.assertEqual(len(passwords), 3)
-        self.assertTrue("root" in passwords.keys())
-        self.assertTrue("backup" in passwords.keys())
-        self.assertTrue("superuser" in passwords.keys())
+        self.assertEqual(len(set(passwords.values())), 3)  # passwords are unique
+        for pwd in passwords.values():
+            self.assertGreaterEqual(len(pwd), 12)
+
+    def test_skipping_admin_user_generation(self):
+        args = Namespace(generate_admins=False, no_passwords=False)
+        client = Mock()
+
+        passwords = {}
+        init_mongo._create_admin_users(args, client, passwords)
+
+        client.admin.command.assert_not_called()
+        self.assertEqual(len(passwords), 0)
+
+    def test_admin_user_generation_without_passwords(self):
+        args = Namespace(generate_admins=True, no_passwords=True)
+        client = Mock()
+
+        passwords = {}
+        init_mongo._create_admin_users(args, client, passwords)
+
+        client.admin.command.assert_called()
+        self.assertEqual(client.admin.command.call_count, 3)
+        self.assertEqual(len(passwords), 3)
+        for user, pwd in passwords.items():
+            self.assertGreaterEqual(user, pwd)
 
 
 if __name__ == '__main__':
