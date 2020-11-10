e40596499d61b870dfe0664b9c99cb9b43db0fc5
Mikko Seppälä <mikko.seppala@gofore.com>
2020-11-10 12:01:59 +0200

    OPMONDEV-25 use tox to run collector unit tests

--- collector_module/setup.py
+++ collector_module/setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 from setuptools import setup
 
+requirements = [
+    "pymongo>=3.4,<4",
+    "requests>=2.24,<3",
+    "numpy>=1.19,<2",
+    "tqdm>=4.14<5",
+    "pyaml>=20.4.0"
+]
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities',
+]
+
+
 setup(name='opmon-collector',
       version='0.1',
       description='X-Road Operational Monitoring Collector Module',
@@ -10,22 +30,8 @@ setup(name='opmon-collector',
       packages=['opmon_collector'],
       package_dir={'opmon_collector': 'opmon_collector'},
       scripts=['bin/opmon-collector'],
-      install_requires=[
-          "pymongo>=3.4,<4",
-          "requests>=2.24,<3",
-          "numpy>=1.19,<2",
-          "tqdm>=4.14<5",
-          "pyaml>=20.4.0"
-      ],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Environment :: Console',
-                   'Intended Audience :: System Administrators',
-                   'License :: OSI Approved :: MIT License',
-                   'Natural Language :: English',
-                   'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 3',
-                   'Topic :: Utilities',
-                   ],
+      install_requires=requirements,
+      classifiers=classifiers,
       platforms='POSIX',
       license='MIT License'
       )
