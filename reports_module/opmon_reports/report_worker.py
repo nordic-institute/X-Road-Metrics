9a790853ef64905b8d61f254f6531efd09acf54b
Mikko Seppälä <mikko.seppala@gofore.com>
2021-01-05 13:46:32 +0200

    OPMONDEV-45 use settings.yaml in report_worker

--- reports_module/opmon_reports/report_worker.py
+++ reports_module/opmon_reports/report_worker.py
@@ -28,67 +28,45 @@ def read_in_json(file_name, logger_manager):
     return data
 
 
-def main(logger_manager):
-    """
-    The main method.
-    :param logger_manager: The LoggerManager object.
-    :return:
-    """
-    # Start timer
-    start_processing_time = time.time()
-
-    args = OpmonReportsArguments()
+def create_report_manager(args, logger_m):
     settings = args.settings
 
-    # Initialize handlers
     db_handler = MongoDBHandler(settings['mongodb'], args.xroad_instance)
-    db_m = DatabaseManager(db_handler, logger_manager)
+    db_m = DatabaseManager(db_handler, logger_m)
 
     # Initialize translator
-    with open(settings.TRANSLATION_FILES.format(LANGUAGE=language), 'rb') as language_file:
+    with open(settings.TRANSLATION_FILES.format(LANGUAGE=args.language), 'rb') as language_file:
         language_template = json.loads(language_file.read().decode("utf-8"))
     translator = Translator(language_template)
 
     # Initialize variables
-    riha_json = read_in_json(settings.SUBSYSTEM_INFO_PATH, logger_manager)
-    meta_services = settings.META_SERVICE_LIST
-    html_template = settings.HTML_TEMPLATE_PATH
-    css_files = settings.CSS_FILES
-    report_path = settings.REPORTS_PATH
-    ria_image_1 = settings.RIA_IMAGE_1.format(LANGUAGE=language)
-    ria_image_2 = settings.RIA_IMAGE_2.format(LANGUAGE=language)
-    ria_image_3 = settings.RIA_IMAGE_3.format(LANGUAGE=language)
+    riha_json = read_in_json(settings.SUBSYSTEM_INFO_PATH, logger_m)
 
     # Generate a report manager
-    report_manager = ReportManager(args,
-                                   riha_json, logger_manager, db_m, meta_services, translator, html_template,
-                                   report_path, css_files, ria_image_1, ria_image_2, ria_image_3)
+    return ReportManager(args, riha_json, logger_m, db_m, translator)
 
-    # Log starting
-    logger_manager.log_info('report_worker', 'report_worker_start')
-    # No need to heartbeat here, it might confuse application monitoring
-    # logger_manager.log_heartbeat(
-    #     "report_worker start", settings.HEARTBEAT_LOGGER_PATH, settings.REPORT_HEARTBEAT_NAME, "SUCCEEDED")
 
-    # Generate report
-    report_manager.generate_report()
+def main():
+    args = OpmonReportsArguments()
+    logger_m = LoggerManager(args.settings['logger'], args.xroad_instance)
 
-    # Log ending of the reports generation
-    end_processing_time = time.time()
-    total_time = time.strftime("%H:%M:%S", time.gmtime(end_processing_time - start_processing_time))
-    logger_manager.log_info('report_worker', 'report_worker_end')
-    logger_manager.log_info('report_worker', 'Total generation time: {0}'.format(total_time))
+    try:
+        start_processing_time = time.time()
 
-    # No need to heartbeat here, it might confuse application monitoring
-    # logger_manager.log_heartbeat(
-    #     "report_worker end", settings.HEARTBEAT_LOGGER_PATH, settings.REPORT_HEARTBEAT_NAME, "SUCCEEDED")
+        report_manager = create_report_manager(args, logger_m)
+        logger_m.log_info('report_worker', 'report_worker_start')
+
+        report_manager.generate_report()
+
+        end_processing_time = time.time()
+        total_time = time.strftime("%H:%M:%S", time.gmtime(end_processing_time - start_processing_time))
+        logger_m.log_info('report_worker', 'report_worker_end')
+        logger_m.log_info('report_worker', 'Total generation time: {0}'.format(total_time))
+    except Exception as e:
+        logger_m.log_error('reports', f'{repr(e)}')
+        logger_m.log_heartbeat("error", "FAILED")
+        raise e
 
 
 if __name__ == '__main__':
-    logger_m = LoggerManager(settings.LOGGER_NAME, 'report_worker')
-    try:
-        main(logger_m)
-    except Exception as e:
-        logger_m.log_error('reports', '{0}'.format(repr(e)))
-        logger_m.log_heartbeat("error", settings.HEARTBEAT_LOGGER_PATH, settings.REPORT_HEARTBEAT_NAME, "FAILED")
-        raise e
+    main()
