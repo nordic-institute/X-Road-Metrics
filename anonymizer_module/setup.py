191d56fe35e6af8e5905211be760a60da32529b5
Mikko Seppälä <mikko.seppala@gofore.com>
2021-01-27 11:48:36 +0200

    OPMONDEV-67 add executable script bin/opmon-anonymizer

--- anonymizer_module/setup.py
+++ anonymizer_module/setup.py
@@ -28,7 +28,7 @@ setup(
     author_email='info@niis.org',
     packages=find_packages(),
     include_package_data=True,
-    # scripts=['bin/opmon-anonymizer'],
+    scripts=['bin/opmon-anonymizer'],
     install_requires=requirements,
     classifiers=classifiers,
     platforms='POSIX',
