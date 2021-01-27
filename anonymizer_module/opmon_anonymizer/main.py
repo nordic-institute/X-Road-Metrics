4d980edb9cd600a5278e3c6f2e6d6d4dcf2c5341
Mikko Seppälä <mikko.seppala@gofore.com>
2021-01-27 11:45:39 +0200

    OPMONDEV-67 rename anonymize.py to main.py and improve handling of command line arguments and exceptions

---
+++ anonymizer_module/opmon_anonymizer/main.py
@@ -0,0 +1,105 @@
+from .anonymizer import Anonymizer
+from .iio.mongodbmanager import MongoDbManager
+from .iio.opendata_writer import OpenDataWriter
+from datetime import datetime
+import sys
+import argparse
+import traceback
+from .settings_parser import OpmonSettingsManager
+from opmon_anonymizer.utils import logger_manager
+
+
+def main():
+    args = parse_args()
+    settings = OpmonSettingsManager(args.profile).settings
+    logger = logger_manager.LoggerManager(settings['logger'], settings['xroad']['instance'])
+
+    try:
+        start_time = datetime.now()
+        record_count = run_anonymizer(settings, logger, args.limit)
+        elapsed = str(datetime.now() - start_time)
+        logger.log_info('anonymization_session_finished',
+                        f"Anonymization finished in {elapsed} seconds. Processed {record_count} entries from MongoDB.")
+    except Exception:
+        pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--profile",
+        metavar="PROFILE",
+        default=None,
+        help="""
+            Optional settings file profile.
+            For example with '--profile PROD' settings_PROD.yaml will be used as settings file.
+            If no profile is defined, settings.yaml will be used by default.
+            Settings file is searched from current working directory and /etc/opmon/corrector/
+        """
+    )
+
+    parser.add_argument(
+        "--limit",
+        default=None,
+        help="""
+            Maximum number of records to anonymize during this run. If this argument is not provided all records
+            available in MongoDb are processed.
+        """
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def setup_reader(settings, logger):
+    try:
+        reader = MongoDbManager(settings)
+        assert reader.is_alive()
+        return reader
+    except Exception:
+        logger.log_heartbeat('Failed to initialize MongoDB connection.', 'FAILED')
+        logger.log_error('mongodb_connection_failed',
+                         f"Failed connecting to MongoDB at {settings['mongodb']['host']}:{settings['mongodb']['port']}")
+        sys.exit(1)
+
+
+def setup_writer(settings, logger):
+    try:
+        writer = OpenDataWriter(settings, logger)
+        assert writer.db_manager.is_alive()
+        return writer
+    except Exception:
+        logger.log_heartbeat('Failed to initialize PostgreSQL connection.', 'FAILED')
+
+        trace = traceback.format_exc().replace('\n', '')
+        logger.log_error('postgresql_connection_failed',
+                         f'Failed initializing postgresql database connector. ERROR: {trace}')
+        sys.exit(1)
+
+
+def run_anonymizer(settings, logger, anonymization_limit):
+    logger.log_info('anonymization_session_started', 'Started anonymization session')
+    logger.log_heartbeat('Started anonymization session', 'SUCCEEDED')
+
+    reader = setup_reader(settings, logger)
+    writer = setup_writer(settings, logger)
+
+    try:
+        logger.log_info('anonymization_process_started', 'Started anonymizing logs.')
+        logger.log_heartbeat('Started anonymizing logs.', 'SUCCEEDED')
+        anonymizer_instance = Anonymizer(reader, writer, settings, logger)
+        records = anonymizer_instance.anonymize(anonymization_limit)
+
+    except Exception:
+        trace = traceback.format_exc().replace('\n', '')
+        logger.log_error('anonymization_process_failed', f'Failed to anonymize. ERROR: {trace}')
+        logger.log_heartbeat('Error occurred during log anonymization', 'FAILED')
+        sys.exit(1)
+
+    logger.log_heartbeat('Finished anonymization session', 'SUCCEEDED')
+    return records
+
+
+if __name__ == "__main__":
+    main()
