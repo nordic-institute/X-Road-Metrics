06d3da97c39dc5b78186aa47703f6ccbc6127b63
Mikko Seppälä <mikko.seppala@gofore.com>
2021-04-06 17:40:35 +0300

    OPMONDEV-53 prototype of extracting parameters from analyzer_conf to settings.yaml

---
+++ analysis_module/opmon_analyzer/constants.py
@@ -0,0 +1,35 @@
+timestamp_field = 'requestInTs'
+
+# These are present on the top level of "clean data" objects
+top_level_column_names = [
+    "_id",
+    'totalDuration',
+    'producerDurationProducerView',
+    'requestNwDuration',
+    'responseNwDuration',
+    'correctorStatus'
+]
+
+# X-Road identifier column names
+# These are present in both client and producer sections of clean data
+service_identifier_column_names = [
+    "clientMemberClass",
+    "clientMemberCode",
+    "clientXRoadInstance",
+    "clientSubsystemCode",
+    "serviceCode",
+    "serviceVersion",
+    "serviceMemberClass",
+    "serviceMemberCode",
+    "serviceXRoadInstance",
+    "serviceSubsystemCode"
+]
+
+#  Request metadata columns in clean data client/producer sections
+service_metadata_column_names = ["succeeded", "messageId", timestamp_field]
+
+# Size column
+size_columns = [
+    {'outputName': 'requestSize', 'clientName': 'clientRequestSize', 'producerName': 'producerRequestSize'},
+    {'outputName': 'responseSize', 'clientName': 'clientResponseSize', 'producerName': 'producerResponseSize'}
+]
