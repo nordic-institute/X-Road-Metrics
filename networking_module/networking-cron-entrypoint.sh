8501625888b60c74525a8c1fe0be98498cc3da81
Raido Kaju <raido.kaju@gmail.com>
2021-10-19 14:27:52 +0300

    Fix wrapper profile handling and update documentation regarding cron command profile usage

--- networking_module/networking-cron-entrypoint.sh
+++ networking_module/networking-cron-entrypoint.sh
@@ -35,7 +35,8 @@ fi
 #
 # Actual stuff
 #
-Rscript /usr/bin/xroad-metrics-networking "$PROFILE_FLAG" 2>&1 | awk '{ print strftime("%Y-%m-%dT%H:%M:%S\t"), $0; fflush(); }' | tee -a ${LOG}
+cd /etc/xroad-metrics/networking
+Rscript /usr/bin/xroad-metrics-networking $PROFILE_FLAG 2>&1 | awk '{ print strftime("%Y-%m-%dT%H:%M:%S\t"), $0; fflush(); }' | tee -a ${LOG}
 
 # Remove ${LOCK}
 /bin/rm ${LOCK} 2>&1 | awk '{ print strftime("%Y-%m-%dT%H:%M:%S\t"), $0; fflush(); }' | tee -a ${LOG}
