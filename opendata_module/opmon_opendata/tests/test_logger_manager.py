ad5e41e881ecd487e390d032f7d4bf06c1063d31
Darius Varnelis <darius.varnelis@cornercasetech.com>
2023-03-02 14:58:37 +0200

    Test opendata_module logger handler is not duplicated

---
+++ opendata_module/opmon_opendata/tests/test_logger_manager.py
@@ -0,0 +1,25 @@
+import logging
+
+from opmon_opendata.logger_manager import LoggerManager
+
+
+def test_logger_manager_handler_is_single(tmp_path):
+    # Regression test for
+    # https://github.com/nordic-institute/X-Road-Metrics/pull/14/files
+
+    temp_log_dir = tmp_path / 'log'
+    temp_log_dir.mkdir()
+
+    settings = {
+        'name': 'test',
+        'module': 'test',
+        'level': 2,
+        'log-path': temp_log_dir,
+        'heartbeat-path': 'test',
+    }
+    LoggerManager(settings, 'test', '1')
+    LoggerManager(settings, 'test', '1')
+    LoggerManager(settings, 'test', '1')
+
+    original_logger = logging.getLogger('test')
+    assert len(original_logger.handlers) == 1
