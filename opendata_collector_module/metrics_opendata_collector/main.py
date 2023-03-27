5ebc8ba7cdcb59a441a31f852ec75ac10fd856b3
Darius Varnelis <darius.varnelis@cornercasetech.com>
2023-03-27 15:50:10 +0300

    [OPMONDEV-161]: create opendata_collector_module and parse settings

---
+++ opendata_collector_module/metrics_opendata_collector/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+#  The MIT License
+#  Copyright (c) 2021- Nordic Institute for Interoperability Solutions (NIIS)
+#  Copyright (c) 2017-2020 Estonian Information System Authority (RIA)
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+import argparse
+
+from metrics_opendata_collector.opendata_collector import collect_opendata
+from metrics_opendata_collector.settings import MetricsSettingsManager
+
+
+def main():
+    args = parse_args()
+    settings_manager = MetricsSettingsManager(args.profile)
+
+    settings = settings_manager.settings
+    open_data_source_settings = settings['opendata-collector']['sources-settings'][args.source_id]
+    collect_opendata(settings, open_data_source_settings)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('source_id',
+                        metavar='SOURCE_ID',
+                        help='X-Road Metrics instances to fetch Open Data from. Taken from sources settings file')
+
+    parser.add_argument('--profile',
+                        metavar='PROFILE',
+                        default=None,
+                        help="""
+                            Optional settings file profile.
+                            For example with '--profile PROD' settings_PROD.yaml will be used as settings file.
+                            If no profile is defined, settings.yaml will be used by default.
+                            Settings file is searched from current working directory and /etc/xroad-metrics/opendata-collector/
+                        """.strip()
+                        )
+    args = parser.parse_args()
+
+    return args
---
+++ opendata_collector_module/metrics_opendata_collector/tests/test_main.py
@@ -0,0 +1,79 @@
+"""
+Unit tests for main.py
+"""
+#  The MIT License
+#  Copyright (c) 2021- Nordic Institute for Interoperability Solutions (NIIS)
+#  Copyright (c) 2017-2020 Estonian Information System Authority (RIA)
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+import pathlib
+import os
+import pytest
+
+import metrics_opendata_collector.main as main
+
+
+SOURCES_SETTINGS = {
+    'TEST-SOURCE1': {
+        'url': 'test url',
+        'limit': 1000,
+    },
+    'TEST-SOURCE2': {
+        'url': 'test url',
+        'limit': 500,
+    },
+}
+
+SETTINGS = {
+    'opendata-collector': {
+        'instance': 'TEST',
+        'sources-settings-path': 'test_opendata_sources_settings.yaml',
+        'sources-settings': SOURCES_SETTINGS,
+    },
+    'mongodb': {
+        'host': 'localhost',
+        'user': 'test_user',
+        'password': 'test_password',
+        'tls': None,
+        'tls-ca-file': None
+    },
+    'logger': {
+        'name': 'opendata-collector',
+        'module': 'opendata-collector',
+        'level': 'INFO',
+        'log-path': '/var/log/xroad-metrics/opendata-collector/logs',
+        'heartbeat-path': '/var/log/xroad-metrics/opendata-collector/heartbeat'
+    }
+}
+
+
+@pytest.fixture
+def set_dir():
+    # take settings files from tests dir
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+
+
+def test_action_collect(mocker, set_dir):
+    mocker.patch('metrics_opendata_collector.main.collect_opendata')
+    mocker.patch('sys.argv', ['test_program_name', '--profile', 'TEST', 'TEST-SOURCE1'])
+    main.main()
+    main.collect_opendata.assert_called_once_with(
+        SETTINGS, SOURCES_SETTINGS['TEST-SOURCE1']
+    )
