a9b63962926b65b67b422eb2d79c8e5b2f87d82f
Darius Varnelis <darius.varnelis@cornercasetech.com>
2023-04-17 19:29:54 +0300

    [Opmondev-162]: use CharField instead of DateTimeField to fix replaced plus sign

--- opendata_module/opmon_opendata/api/forms.py
+++ opendata_module/opmon_opendata/api/forms.py
@@ -3,39 +3,69 @@ import json
 
 from django import forms
 
-# Django 2.2 does not support this format by default
-INPUT_FORMATS = [
-    '%Y-%m-%dT%H:%M:%S%z',
-]
+DT_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
 
 
 class HarvestForm(forms.Form):
-    from_dt = forms.DateTimeField(required=True, input_formats=INPUT_FORMATS)
-    until_dt = forms.DateTimeField(required=False, input_formats=INPUT_FORMATS)
+    from_dt = forms.CharField(required=True)
+    until_dt = forms.CharField(required=False)
     offset = forms.IntegerField(required=False)
     limit = forms.IntegerField(required=False, min_value=0)
     from_row_id = forms.IntegerField(required=False)
     # compatibility with Django 2.2
     order = forms.CharField(required=False)
 
+    @staticmethod
+    def get_timestamp_with_tz_offset(timestamp):
+        timestamp = timestamp.replace(' ', '+')
+        return timestamp
+
     def clean(self):
+        from_dt = None
+        until_dt = None
         cleaned_data = super().clean()
-        from_dt = cleaned_data.get('from_dt')
-        until_dt = cleaned_data.get('until_dt')
-        if from_dt and until_dt:
+        from_dt_str = cleaned_data.get('from_dt')
+        if from_dt_str:
+            from_dt = datetime.datetime.strptime(from_dt_str, DT_FORMAT)
+        until_dt_str = cleaned_data.get('until_dt')
+        if from_dt_str and until_dt_str:
+            until_dt = datetime.datetime.strptime(until_dt_str, DT_FORMAT)
             if from_dt > until_dt:
-                self.add_error('from_dt', 'Ensure the value is not later than until_dt')
-                self.add_error('until_dt', 'Ensure the value is not earlier than from_dt')
+                self.add_error(
+                    'from_dt', 'Ensure the value is not later than until_dt'
+                )
+                self.add_error(
+                    'until_dt', 'Ensure the value is not earlier than from_dt'
+                )
+        cleaned_data['from_dt'] = from_dt
+        cleaned_data['until_dt'] = until_dt
         return cleaned_data
 
     def clean_from_dt(self):
         now = datetime.datetime.now(datetime.timezone.utc)
         from_dt_value = self.cleaned_data['from_dt']
-        if now < from_dt_value:
+        from_dt_value = HarvestForm.get_timestamp_with_tz_offset(from_dt_value)
+        try:
+            from_dt = datetime.datetime.strptime(from_dt_value, DT_FORMAT)
+        except ValueError:
+            self.add_error('from_dt', f'Ensure the value matches format {DT_FORMAT}')
+            return
+        if now < from_dt:
             raise forms.ValidationError(
                 'Ensure the value is not later than the current date and time'
             )
         return from_dt_value
+
+    def clean_until_dt(self):
+        until_dt_value = self.cleaned_data['until_dt']
+        if until_dt_value:
+            until_dt_value = HarvestForm.get_timestamp_with_tz_offset(until_dt_value)
+            try:
+                datetime.datetime.strptime(until_dt_value, DT_FORMAT)
+            except ValueError:
+                self.add_error('until_dt', f'Ensure the value matches format {DT_FORMAT}')
+                return
+        return until_dt_value
 
     def clean_order(self):
         ALLOWED_ORDER_KEYS = ['column', 'order']
