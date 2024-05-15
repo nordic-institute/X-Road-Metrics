dd8ca19dee4af71e23c2528280d6a46cfbf99865
Mohamed Elbeltagy <mohamed.elbeltagy@nortal.com>
2024-05-15 04:09:14 +0300

    feat: Update run_tests.sh command to use ruff and run tests
    Refs: OPMONDEV-181

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # pip install pytest
-# pip install flake8
 # pip install ruff
 
 export REPORTS_RUN_ENV="REPORTS_TEST"
@@ -9,20 +8,24 @@ export REPORTS_RUN_ENV="REPORTS_TEST"
 #set -e
 
 # Run Pep8 Tests
+#ruff check analysis_module
+#ruff check analysis_ui_module
+ruff check anonymizer_module
 ruff check collector_module
 ruff check corrector_module
-ruff check reports_module/opmon_reports/report_manager.py
-#ruff check analysis_ui_module
-#ruff check analysis_module
-ruff check anonymizer_module
+ruff check opendata_collector_module
+ruff check opendata_module
+ruff check reports_module
 
 ## Run Tests
-pytest --cache-clear collector_module --html=collector_module/test_results.html --self-contained-html
-pytest --cache-clear corrector_module --html=corrector_module/test_results.html --self-contained-html
-pytest --cache-clear reports_module --html=reports_module/test_results.html --self-contained-html
-#pytest --cache-clear analysis_ui_module --html=analysis_ui_module/test_results.html --self-contained-html
-#pytest --cache-clear analysis_module --html=analysis_module/test_results.html --self-contained-html
-pytest --cache-clear opendata_module/anonymizer --html=opendata_module/anonymizer/test_results.html --self-contained-html
+#pytest --cache-clear --ruff analysis_module --html=analysis_module/test_results.html --self-contained-html
+#pytest --cache-clear --ruff analysis_ui_module --html=analysis_ui_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff anonymizer_module --html=anonymizer_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff collector_module --html=collector_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff corrector_module --html=corrector_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff opendata_collector_module --html=opendata_collector_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff opendata_module --html=opendata_module/test_results.html --self-contained-html
+pytest --cache-clear --ruff reports_module --html=reports_module/test_results.html --self-contained-html
 
 # Run CI Tests
 if [[ $1 == 'CI' ]] ; then
