2bd61f536922c3133991b4c948f8d17c036d9570
Mohamed Elbeltagy <mohamed.elbeltagy@nortal.com>
2026-02-09 15:52:41 +0200

    fix: Downgrade opendata docker to python 3.10 to match other modules

--- Docker/prepare-containers.sh
+++ Docker/prepare-containers.sh
@@ -26,7 +26,6 @@ AMD64_ONLY_MODULES=(networking_module)
 
 get_base_image() {
   case "$1" in
-    opendata_module)   echo "3.10-slim-bookworm" ;;
     networking_module) ;;
     *)                 echo "3.10-slim" ;;
   esac
